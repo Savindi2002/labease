31cac7cbe1e7b6f596b2b908a37f69ab709ceb7d
uvinduuvidura <hewagesavindi@gmail.com>
2025-11-01 10:34:23 +0530

    added admin feauters

---
+++ admin.php
@@ -0,0 +1,4 @@
+<?php include 'db_connect.php'; session_start(); ?>
+<h2>Welcome Admin</h2>
+<a href="manage_labs.php">Manage Labs</a><br>
+<a href="logout.php">Logout</a>
---
+++ student.php
@@ -0,0 +1,4 @@
+<?php include 'db_connect.php'; session_start(); ?>
+<h2>Welcome Student</h2>
+<a href="booking.php">Book a Lab</a><br>
+<a href="logout.php">Logout</a>
